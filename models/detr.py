2f1311dc20c03582ffdc8993e39f16200a8fe37b
cox0902-mini <cox0902@163.com>
2025-08-31 17:52:43 +0800

    __

--- models/detr.py
+++ models/detr.py
@@ -259,7 +259,7 @@ class SetCriterion(nn.Module):
         # In case of auxiliary losses, we repeat this process with the output of each intermediate layer.
         if 'aux_outputs' in outputs:
             for i, aux_outputs in enumerate(outputs['aux_outputs']):
-                indices = self.matcher(aux_outputs, targets)
+                # indices = self.matcher(aux_outputs, targets)
                 for loss in self.losses:
                     if loss == 'masks':
                         # Intermediate masks losses are too costly to compute, we ignore them.
